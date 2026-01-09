2c797517dbe0d836ad575a10435ba8c018b62a31
Yusuf-Jaiteh <yusufjaiteh77@gmail.com>
2026-01-09 21:40:12 +0100

    adjusted the frontend url

--- DFMP_BACKEND/src/main/java/org/example/Security/SecurityConfig.java
+++ DFMP_BACKEND/src/main/java/org/example/Security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "dfmp.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://dfmp.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
